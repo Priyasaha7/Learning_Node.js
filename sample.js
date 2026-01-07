cb5f1d26e112d7eeaceb98c21f25c6df0fc68489
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-07 20:47:32 +0530

    Add detailed explanation of Node.js module wrapper and require() internals
    - Explained IIFE-based module isolation
    - Documented Node.js wrapper function from source code
    - Added require() resolution, loading, execution & caching steps
    - Clarified role of Node.js vs V8
    - Improved notes with diagrams and examples

--- sample.js
+++ sample.js
@@ -1,1 +1,25 @@
-console.log("very important file");
+// console.log("very important file");
+
+// when we writing require ("./path") it is exactly doing the same thing , wrap our code in an anonoumous function
+
+
+// IIFE - immediately invoked function expression 
+// (function (module , require) {
+    // All the code of the module runs inside here
+// })
+
+console.log(__filename);
+
+console.log(__dirname);
+
+
+// let wrap = function (script) { // eslint-disable-line func-style
+//   return Module.wrapper[0] + script + Module.wrapper[1];
+// };
+
+// const wrapper = [
+//   '(function (exports, require, module, __filename, __dirname) { ',
+//   '\n});',
+// ];
+
+// basically doing string concatenation
