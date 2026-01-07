2517c42ac678684dd59b4c28f5afa9d2bd15c354
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-07 16:05:55 +0530

    first commit-write the basics

---
+++ app.js
@@ -0,0 +1,15 @@
+var name = "Priya Saha"
+
+var a = 10 
+var b = 30
+
+// console.log(a+b);
+// console.log(name);
+
+// console.log(global);
+// console.log(globalThis);
+
+console.log(global === globalThis);
+
+console.log(this);
+// this will return empty 
